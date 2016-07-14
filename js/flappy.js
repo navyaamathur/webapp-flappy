2f734b3cbd5f43ad6304a064537b3071b34401dd
Navyaa Mathur <navyaamathur@yahoo.com>
2016-07-14 09:57:23 +0100

    hopefully this one gets online

--- js/flappy.js
+++ js/flappy.js
@@ -1,85 +1,171 @@
-// the Game object used by the phaser.io library
 var stateActions = { preload: preload, create: create, update: update };
-
-// Phaser parameters:
-// - game width
-// - game height
-// - renderer (go for Phaser.AUTO)
-// - element where the game will be drawn ('game')
-// - actions on the game state (or null for nothing)
-var game = new Phaser.Game(790, 400, Phaser.AUTO, 'game', stateActions);
+var width = 800;
+var height = 400;
+var gameGravity = 250;
+var gameSpeed = 175;
+var jumpPower = 200;
+var game = new Phaser.Game(width, height, Phaser.AUTO, 'game', stateActions);
 var player;
 var pipes = [];
+var coins = [];
+var balloons = [];
+var weight = [];
 var score = -3;
 var scorer = 0;
 var labelScore;
+var gapSize = 180;
+var gapMargin = 50;
+var blockHeight = 50;
+var splashDisplay;
 
-//Loads all resources for the game and gives them names.
 function preload() {
-    game.load.image("playerImage", "../assets/star.png");
+    game.load.image("coin", "../assets/coin.png");
+    game.load.image("playerImage", "../assets/flappy.png");
     game.load.audio("score", "../assets/point.ogg");
     game.load.image("pipeBlock", "../assets/pipe.png");
+    game.load.image("pipeEnd", "../assets/pipe-end.png");
+    game.load.image("balloons", "../assets/balloons.png");
+    game.load.image("weight", "../assets/weight.png");
+  }
+
+function start(){
+  splashDisplay.destroy();
+  game.input
+      .keyboard.addKey(Phaser.Keyboard.ENTER)
+      .onDown.remove(start);
+  game.input
+      .keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+      .onDown.add(function()
+    {
+      player.body.velocity.y = -jumpPower;
+    });
+  labelScore = game.add.text(750, 360, "0");
+  player.body.gravity.y = gameGravity;
+  var pipeInterval = 1.75 * Phaser.Timer.SECOND;
+  game.time.events.loop(pipeInterval, generate);
 }
 
-
-//Initialises the game. This function is only called once.
 function create() {
-    // set the background colour of the scene
-    game.stage.setBackgroundColor("#00FFFF");
-    //game.add.text(20, 20, "Welcome", {font: "25px Courier", fill: "#FF3385"});
-    //game.input.onDown.add(clickHandler);
-    game.input
-        .keyboard.addKey(Phaser.Keyboard.SPACEBAR)
-        .onDown.add(playerJump);
-    //alert(score);
-    player = game.add.sprite(10, 200, "playerImage");
-    labelScore = game.add.text(750, 360, "0");
-    player.scale.setTo(0.5, 0.5);
-    generatePipe();
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.physics.arcade.enable(player);
-    //player.body.velocity.x = 100;
-    player.body.gravity.y = 250;
-    var pipeInterval = 1.75 * Phaser.Timer.SECOND;
-    game.time.events.loop(pipeInterval, generatePipe);
+  game.stage.setBackgroundColor("#00FFFF");
+  player = game.add.sprite(30, 200, "playerImage");
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.arcade.enable(player);
+  player.anchor.setTo(0.5, 0.5);
+  player.scale.setTo(0.8, 0.8);
+  splashDisplay = game.add.text(100, 200, "Press ENTER to start.");
+  game.input
+      .keyboard.addKey(Phaser.Keyboard.ENTER)
+      .onDown.add(start);
 }
 
-function generatePipe(){
-var gapStart = game.rnd.integerInRange(1, 5);
-for (var count = 0; count < 8; count++){
-if(count != gapStart && count != gapStart+1){
-addPipeBlock(750, count*50);}}
-changeScore();
+function update() {
+player.rotation = Math.atan(player.body.velocity.y / (gameSpeed*3));
+game.physics.arcade.overlap(player, pipes, gameOver);
+effect(-50, balloons);
+effect(50, weight);
+scoreUp();
+if(player.y>360){gameOver();}
+if(player.y<0){gameOver();}
 }
 
 function addPipeBlock(x, y){
   var block = game.add.sprite(x, y, "pipeBlock");
   pipes.push(block);
   game.physics.arcade.enable(block);
-  block.body.velocity.x = -175;
+  block.body.velocity.x = -gameSpeed;
 }
 
-function playerJump(){
-  player.body.velocity.y = -200;
+function addPipeEnd(x, y){
+  var end = game.add.sprite(x, y, "pipeEnd");
+  pipes.push(end);
+  game.physics.arcade.enable(end);
+  end.body.velocity.x = -gameSpeed;
 }
 
+function addCoin(x, y){
+  var coin = game.add.sprite(x, y, "coin");
+  coin.scale.setTo(0.25, 0.25);
+  coins.push(coin);
+  game.physics.arcade.enable(coin);
+  coin.body.velocity.x = -gameSpeed;
+}
 
-//This function updates the scene. It is called for every new frame.
-function update() {
-game.physics.arcade.overlap(player, pipes, gameOver);
-if(player.y>360){gameOver();}
-if(player.y<0){gameOver();}
+function scoreUp(){
+  for(var i in coins){
+    game.physics.arcade.overlap(player, coins[i], function(){
+      changeScore();
+      coins[i].destroy();
+      //coins.splice(i, 1);
+    });
+  }
+}
+
+function effect(effectNumber, effectArray){
+  for(var n = effectArray.length; n>0; n--){
+    game.physics.arcade.overlap(player, effectArray[n], function(){
+      gameGravity+=effectNumber;
+      player.body.gravity.y = gameGravity;
+      effectArray[n].destroy();
+      effectArray.splice(n, 1);
+    });
+  }
 }
 
 function changeScore(){
-    score++;
-    if(score<0){scorer=0;}
-    else{scorer=score;}
+    scorer++;
+    //if(score<0){scorer=0;}
+    //else{scorer=score;}
     labelScore.setText(scorer.toString());
   }
 
 function gameOver(){
   player.kill();
-  registerScore(score);
+  registerScore(scorer);
+  score=-3;
+  scorer=0;
+  gameGravity = 250;
+  coins = [];
   game.state.restart();
+}
+
+function generateBalloons(){
+  var bonus = game.add.sprite(width, height, "balloons");
+  balloons.push(bonus);
+  game.physics.arcade.enable(bonus);
+  bonus.body.velocity.x = -gameSpeed;
+  bonus.body.velocity.y = -game.rnd.integerInRange(50, 80);
+}
+
+function generateWeight(){
+  var bonus = game.add.sprite(width, 50, "weight");
+  balloons.push(bonus);
+  game.physics.arcade.enable(bonus);
+  bonus.body.velocity.x = -gameSpeed;
+  bonus.body.velocity.y = game.rnd.integerInRange(50, 80);
+}
+
+function generatePipe(){
+  var gapStart = game.rnd.integerInRange(gapMargin, height-gapSize-gapMargin);
+  for(var y = gapStart; y>0; y-=blockHeight){
+    addPipeBlock(width, y-blockHeight);
+  }
+  for(var z = gapStart + gapSize; z<height; z+=blockHeight){
+    addPipeBlock(width, z);
+  }
+  addPipeEnd(width-5, gapStart);
+  addPipeEnd(width-5, gapStart+gapSize-25);
+  addCoin(width+30, gapStart+10);
+}
+
+function generate(){
+  var type = game.rnd.integerInRange(1, 8);
+  if(type == 1){
+    generateWeight();
+  }
+  else if(type==2){
+    generateBalloons();
+  }
+  else{
+    generatePipe();
+  }
 }
--- js/interactivity.js
+++ js/interactivity.js
@@ -1,8 +1,50 @@
 var truthi = true;
 var truthc = true;
 var truths = true;
+var highScoresNumber = [0, 0, 0, 0, 0];
+var highScores = [" "," "," "," "," "];
 var scoreRecord = "";
 var instructing = "<p>Instructions:</p><ul><li>Tap the spacebar to jump.</li>Pipes kill.<li>Scores NOT recorded.</li><li>Will restart after you die.</li></ul>";
+
+function registerScore(scorer){
+  var highscoreFlag = false;
+  for(var i in highScoresNumber){
+    if(scorer>highScoresNumber[i]){
+      highscoreFlag = true;
+      highScoresNumber.splice(i, 0, scorer);
+      highScoresNumber.pop();
+      var playerName = prompt("What's your name?");
+      var scoreEntry = "<li>" + playerName + ":" + scorer.toString() + "</li>";
+      highScores.splice(i, 0, scoreEntry);
+      highScores.pop();
+      break;
+    }
+    //if(scorer<=highScoresNumber[i]){
+      //alert("Sorry, you didn't make the scoreboard!");
+    //}
+    /*else if (scorer==i) {
+      highScoresNumber.splice(highScoresNumber[i], 0, scorer);
+      var playername = prompt("What's your name?");
+      var scoreentry = "<li>" + playername + ":" + scorer.toString() + "</li>";
+      highScores.splice(highScores[i], 0, scoreentry);}*/
+  }
+
+  if (!highscoreFlag) {
+    alert("Sorry, you didn't make the scoreboard!");
+  }
+
+  scoreRecord = "";
+  for(var r in highScores){
+    if(highScores[r] != " "){
+      scoreRecord = scoreRecord + highScores[r];
+    }
+  }
+
+  if(!truths) {
+    jQuery("#content").empty();
+    jQuery("#content").append("<ol>"+scoreRecord+"</ol>");
+  }
+}
 
 jQuery("#credits").on("click", function()
 {if(truthc){
@@ -21,17 +63,11 @@ jQuery("#instructions").on("click", function(){if(truthi){
   ); truthi=false;truthc=true;truths=true;}
 else{jQuery("#content").empty(); truthi=true;}});
 
-function registerScore(){
-  var playerName = prompt("What's your name?");
-  var scoreEntry = "<li>" + playerName + ":" + score.toString() + "</li>";
-  scoreRecord = scoreRecord + scoreEntry;
-}
-
 jQuery("#score").on("click", function()
 {
   if(truths){
     jQuery("#content").empty();
-    jQuery("#content").append(scoreRecord);
+    jQuery("#content").append("<ol>"+scoreRecord+"</ol>");
     truths=false;truthi=true;truthc=true;
   }
   else{jQuery("#content").empty();truths=true;}
